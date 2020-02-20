52490d485c26e417971441f7bfce1e13ce862a07
Fabian Born <fabian.born@fabianborn.net>
2020-02-20 07:00:46 +0100

    Update 20.02.2020:07:00:46

--- azure.tf
+++ azure.tf
@@ -15,8 +15,8 @@ resource "azurerm_subnet" "subnet" {
     name           = "GatewaySubnet"
     resource_group_name  = azurerm_resource_group.dfdemo.name
     virtual_network_name = azurerm_virtual_network.dfdemo.name
-    #address_prefix = "172.16.0.0/24"
-    address_prefix =  "${var.az_gatewaysubnet_cidr}"
+    address_prefix = "172.16.0.0/24"
+    #address_prefix =  "${var.az_gatewaysubnet_cidr}"
   }
 
 
