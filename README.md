fdf3a9dfe529d42c35a6d1087fef6c25e262a089
Fabian Born <fabian.born@fabianborn.net>
2020-02-21 16:05:13 +0100

    Update 21.02.2020:16:05:13

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 ## Deploy a VPN between GCP and Azure
 
+First you have to create the credential.tf
+`
+provider "azurerm" {
+    subscription_id = "<subscription id>"
+    tenant_id       = "<tenant id>"
+}
+
+provider "google" {
+  project = "<project name>"
+  region  = "<region>"
+  zone    = "<region-zone>"
+  credentials = "${file("account.json")}"
+}
+`
--- azure.tf
+++ azure.tf
@@ -7,30 +7,41 @@ resource "azurerm_virtual_network" "dfdemo" {
   name                = "dfdemo_network"
   location            = azurerm_resource_group.dfdemo.location
   resource_group_name = azurerm_resource_group.dfdemo.name
-  #address_space       = ["172.16.0.0/16"]
-  address_space       = "${var.az_network_cidr}"
+  address_space       = ["${var.az_network_cidr}"]
 
 }
 resource "azurerm_subnet" "subnet" {
     name           = "GatewaySubnet"
     resource_group_name  = azurerm_resource_group.dfdemo.name
     virtual_network_name = azurerm_virtual_network.dfdemo.name
-    address_prefix = "172.16.0.0/24"
-    #address_prefix =  "${var.az_gatewaysubnet_cidr}"
+    address_prefix =  var.az_gatewaysubnet_cidr
   }
 
-
+resource "azurerm_subnet" "netsubnet" {
+    name           = "network1Subnet"
+    resource_group_name  = azurerm_resource_group.dfdemo.name
+    virtual_network_name = azurerm_virtual_network.dfdemo.name
+    address_prefix =  var.az_subnet1_cidr
+  }
 resource "azurerm_public_ip" "vpn" {
   name                = "dfdemo_vpn_pubip"
   location            = azurerm_resource_group.dfdemo.location
   resource_group_name = azurerm_resource_group.dfdemo.name
-
   allocation_method = "Dynamic"
 }
 
+data "azurerm_public_ip" "vpn" {
+  name                = azurerm_public_ip.vpn.name
+  resource_group_name = azurerm_public_ip.vpn.resource_group_name
+  depends_on = [azurerm_public_ip.vpn]
+}
+
+output "public_ip_address" {
+  value = data.azurerm_public_ip.vpn.ip_address
+}
 
 resource "azurerm_virtual_network_gateway" "dfdemo_vpn" {
-  name                = "test"
+  name                = "dfdemo_az_gateway"
   location            = azurerm_resource_group.dfdemo.location
   resource_group_name = azurerm_resource_group.dfdemo.name
 
@@ -49,22 +60,22 @@ resource "azurerm_virtual_network_gateway" "dfdemo_vpn" {
   }
 }
 
-#resource "azurerm_local_network_gateway" "gcp" {
-#  name                = "gcpgateway"
-#  resource_group_name = azurerm_resource_group.dfdemo.name
-#  location            = azurerm_resource_group.dfdemo.location
-#  gateway_address     = google_compute_address.vpn_static_ip.address
-#  address_space       = google_compute_network.gcp-network.name
-#}
+resource "azurerm_local_network_gateway" "gcp" {
+  name                = "dfdemo_gcp_gateway"
+  resource_group_name = azurerm_resource_group.dfdemo.name
+  location            = azurerm_resource_group.dfdemo.location
+  gateway_address     = google_compute_address.vpn_static_ip.address
+  address_space       = ["${var.gcp_subnet1_cidr}"]
+}
 
-#resource "azurerm_virtual_network_gateway_connection" "gcp" {
-#  name                = "gcp"
-#  location            = azurerm_resource_group.dfdemo.location
-#  resource_group_name = azurerm_resource_group.dfdemo.name
+resource "azurerm_virtual_network_gateway_connection" "gcp" {
+  name                = "dfdemo_gcp_connection"
+  location            = azurerm_resource_group.dfdemo.location
+  resource_group_name = azurerm_resource_group.dfdemo.name
 
-#  type                       = "IPsec"
-#  virtual_network_gateway_id = azurerm_virtual_network_gateway.dfdemo_vpn.id
-#  local_network_gateway_id   = azurerm_local_network_gateway.gcp.id
+  type                       = "IPsec"
+  virtual_network_gateway_id = azurerm_virtual_network_gateway.dfdemo_vpn.id
+  local_network_gateway_id   = azurerm_local_network_gateway.gcp.id
 
-#  shared_key = random_string.password.result
-#}
+  shared_key = random_string.password.result
+}
